5da7ef796c5464fc79536f03eac2704dc17c26fb
Jun <peng940825@gmail.com>
2026-04-03 19:32:17 +0800

    feat: add dark mode toggle and custom favicon

---
+++ components/theme-toggle.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { Sun, MoonStar } from "lucide-react";
+import { useState, useEffect, startTransition } from "react";
+
+import { Button } from "@/components/ui/button";
+
+const ThemeToggle = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    startTransition(() => setMounted(true));
+  }, []);
+
+  return (
+    mounted && (
+      <Button
+        size="icon"
+        variant="secondary"
+        className="absolute right-3 bottom-3"
+        onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+      >
+        {resolvedTheme === "dark" ? <MoonStar /> : <Sun />}
+      </Button>
+    )
+  );
+};
+
+export default ThemeToggle;
